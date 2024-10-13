d82034653798dddd67650b67586df229791dd96e
kurylko <reviliya@gmail.com>
2024-10-13 19:50:35 +0200

    set apollo client in react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
-import React from "react";
-import logo from './logo.svg';
 import './App.css';
+import {ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from} from "@apollo/client";
+import {onError} from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+        graphQLErrors.forEach(({ message, locations, path }) => {
+            console.error(
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+            );
+        });
+    }
+    if (networkError) {
+        console.error(`[Network error]: ${networkError}`);
+    }
+});
+
+const link = from([
+    errorLink,
+    new HttpLink({uri: "http://localhost:6969/graphql"})
+]);
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: link,
+});
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-
-
-
-  React.useEffect(() => {
-    fetch("/api")
-        .then((res) => res.json())
-        .then((data) => setData(data.message));
-  }, []);
 
   return (
+      <ApolloProvider client={client}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{!data ? "Loading something ..." : data}</p>
-        </header>
+          <h1>Hello!</h1>
       </div>
+      </ApolloProvider>
   );
 }
 
